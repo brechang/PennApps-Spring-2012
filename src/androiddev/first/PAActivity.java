28d4ddea3ce9703b60e9dd40c46efdb665b967a7
Yukuan Zhang <zoingy@gmail.com>
2012-01-13 20:49:06 -0500

    First actual thing

---
+++ src/androiddev/first/PAActivity.java
@@ -0,0 +1,13 @@
+package androiddev.first;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PAActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
